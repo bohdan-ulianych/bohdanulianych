f2ece224447364f4add4d0296f7c4614b3133b65
Bohdan <antoshkayepta@gmail.com>
2021-06-03 21:32:52 +0300

    work5_catsAndPlates_implemented

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+    private boolean satiety;
+    Cat(String name, int appetite) {
+
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getAppetite() {
+        return appetite;
+    }
+
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+
+
+
+
+
+    void eat(Plate p) {
+        p.decreaseFood(appetite);
+        satiety = true;
+    }
+}
