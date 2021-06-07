f0e3614dded4cdb4ab0371f94650b5f6b0f1f0fa
Bohdan <antoshkayepta@gmail.com>
2021-06-07 11:47:49 +0300

    work7_MyArraySizeExeption

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,40 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) throws MyArraySizeException {
+        String mass[][] = new String[][]{{"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}};
+        String mass1[][] = new String[][]{{"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}};
+
+        try {
+            checkArr(mass1);
+
+        } catch (MyArraySizeException e) {
+            System.out.println("Подан масив другого размера");
+        }
+
+
+    }
+
+
+    public static void checkArr(String[][] arr) throws MyArraySizeException {
+        if (arr.length == 4) {
+            System.out.println("Подали масив размером 4х4");
+        } else {
+            throw new MyArraySizeException();
+        }
     }
 }
+
+
+
+
+
+
+
+
