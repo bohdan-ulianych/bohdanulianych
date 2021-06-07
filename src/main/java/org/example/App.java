b89c53208dc57ca3a0cc365d9940666ce139f71c
Bohdan <antoshkayepta@gmail.com>
2021-06-07 12:25:26 +0300

    work7_implemented

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,32 +2,62 @@ package org.example;
 
 
 public class App {
-    public static void main(String[] args) throws MyArraySizeException {
-        String mass[][] = new String[][]{{"1", "2", "3", "4"},
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        String mass[][] = new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}};
+        String mass1[][] = new String[][]{
+                {"1", "2", "3", "4"},
+                {"1", "ячейка 1.1", "3", "4"},
+                {"1", "2", "3", "4"},
+                {"1", "2", "3", "4"}};
+        String mass2[][] = new String[][]{
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"},
                 {"1", "2", "3", "4"}};
-        String mass1[][] = new String[][]{{"1", "2", "3", "4"},
-                {"1", "2", "3", "4"},
-                {"1", "2", "3", "4"}};
-
+        String mass3[][] = new String[][]{
+                {"1", "2", "3"},
+                {"1", "2", "3"},
+                {"1", "2", "3"},
+                {"5", "2", "3"}};
         try {
-            checkArr(mass1);
-
-        } catch (MyArraySizeException e) {
-            System.out.println("Подан масив другого размера");
+            try {
+                int result = checkArr(mass);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива не 4х4!");
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка при приоброзовании массива. Обратите внимание на ячейку: [" + e.i + "][" + e.j + "]");
         }
-
-
     }
 
 
-    public static void checkArr(String[][] arr) throws MyArraySizeException {
-        if (arr.length == 4) {
-            System.out.println("Подали масив размером 4х4");
-        } else {
+    public static int checkArr(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int summ = 0;
+        if (arr.length != 4) {
             throw new MyArraySizeException();
         }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) throw new MyArraySizeException();
+            for (int j = 0; j < arr[i].length; j++) {
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    summ = summ + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return summ;
     }
 }
 
