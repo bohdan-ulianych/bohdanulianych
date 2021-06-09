9d3c7306a3291c327f416edd580ff66aa6b7ff59
Bohdan <antoshkayepta@gmail.com>
2021-06-09 17:17:28 +0300

    work9_collections_implemented

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,61 @@
 package org.example;
+import java.util.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /////////////////////////////task1
+        List<String> mas = Arrays.asList(
+                "fox", "cat", "woolf", "cat", "chicken",
+                "bird", "bear", "fox", "fox", "fox",
+                "sheep", "woolf", "woolf", "woolf", "woolf",
+                "kitten", "cat", "Chupakabra", "cat", "cat"
+        );
+
+        Set<String> unique = new HashSet<String>(mas);
+
+        System.out.println("Start array: ");
+        System.out.println(mas.toString());
+        System.out.println("Unique words");
+        System.out.println(unique.toString());
+        System.out.println("Number of occurrence words: ");
+        for (String key : unique) {
+            System.out.print(key + ": " + Collections.frequency(mas, key)+ ", ");
+        }
+        System.out.println("");
+        ////////////////task2
+        System.out.println("Add a new phonebook...");
+        Phonebook phonebook = new Phonebook();
+        System.out.println("");
+        System.out.println("Add marks into phonebook...");
+        phonebook.add("Frodo", "06669885698");
+        phonebook.add("Harry Potter", "02225845");
+        phonebook.add("Sponge Bob", "2499");
+        phonebook.add("Mikel Jordan", "16516131651");
+        phonebook.add("Lionel Messi", "541651");
+        System.out.println("");
+
+        System.out.println("Find data...");
+        System.out.println("Frodo");
+        System.out.println(phonebook.get("Frodo"));
+        System.out.println("Sponge Bob");
+        System.out.println(phonebook.get("Sponge Bob"));
+        System.out.println("Lionel Messi");
+        System.out.println(phonebook.get("Lionel Messi"));
+        System.out.println("");
+
+        System.out.println("If we don`t have coincidences...");
+        System.out.println("Thor");
+        System.out.println(phonebook.get("Thor"));
+        System.out.println("-");
+
+        System.out.println("Try to add existing number...");
+        phonebook.add("Frodo", "22334465454");
+        System.out.println("Frodo");
+        System.out.println(phonebook.get("Frodo"));
+
     }
-}
+    }
+
+
